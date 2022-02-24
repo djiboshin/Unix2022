6f045534ab37b69bb8854e608684038a40f5f148
djiboshin <test@DESKTOP-P3MH609.localdomain>
2022-02-24 22:43:09 +0300

    script fix

--- new_lab_folders.sh
+++ new_lab_folders.sh
@@ -5,7 +5,10 @@ if [ -n "$1" ]; then
 		exit 1
 	else
 		dir_name=$(printf "lab_%02d" $1)
-		mkdir -p "$dir_name/build" "$dir_name/src" "$dir_name/doc" "$dir_name/cmake"
+		mkdir -p "$dir_name/build" && touch "$_/.gitkeep"
+		mkdir -p "$dir_name/src" && touch "$_/.gitkeep"
+		mkdir -p "$dir_name/doc" && touch "$_/.gitkeep"
+		mkdir -p "$dir_name/cmake" && touch "$_/.gitkeep"
 		exit 0
 	fi
 else
